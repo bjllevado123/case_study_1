444c84741794c70bbc66f472af912a90217f114d
bjllevado123 <llevadob@gmail.com>
2022-10-20 22:34:29 +0800

    body width
    change from 90vw to 100vw

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/css/stylesheet.css?version=v9">
+    <link rel="stylesheet" href="assets/css/stylesheet.css?version=10">
     <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.css">
 </head>
 <body>
